08e664a549ed0818d8309ebffa06cf71ba2058ef
Feyyaz Can KÖSE <kesemenere123@gmail.com>
2024-10-18 01:47:04 +0300

    Add ServiceResult

--- obj/Debug/net8.0/SsttekAcademyHomeWork.AssemblyInfo.cs
+++ obj/Debug/net8.0/SsttekAcademyHomeWork.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SsttekAcademyHomeWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe698b4750d07bfbe9674d4a9f678c236a4201d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60249cdc8bebabeffd81450074c1933204123cb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SsttekAcademyHomeWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SsttekAcademyHomeWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
